ed8cc112ed8ca72c84bff8ef02bf842dcb1da48b
faldhighifari <faldhighifari248@gmail.com>
2022-03-09 10:36:32 +0700

    tahap 2

--- project.js
+++ project.js
@@ -8,6 +8,39 @@ function addBlog() {
     let start = document.getElementById('input-start-date').value;
     let end = document.getElementById('input-end-date').value;
 
+    console.log('awal :', start)
+    console.log('akhir :', end)
+
+    // let dis = new Date(end) - new Date(start);
+    // console.log(dis)
+
+    let nodejs = document.getElementById("tech1");
+    let reactjs = document.getElementById("tech2");
+    let nextjs = document.getElementById("tech3");
+    let typescript = document.getElementById("tech4");
+    
+    let res = "";
+    
+      if (nodejs.checked == true){
+        let pl1 = document.getElementById("tech1").value;
+        res += '<img value="nodejs" id="tech1" src="assets/nodejs.svg" style = "width: 8%;" alt=""> '
+      } 
+      if (reactjs.checked == true){
+        let pl2 = document.getElementById("tech2").value;
+        res +='<img value="reactjs" id="tech2" src="assets/reactjs.svg" style = "width: 8%;" alt=""> '
+      }
+      if (nextjs.checked == true){
+        let pl3 = document.getElementById("tech3").value;
+        res += '<img value="nextjs" id="tech3" src="assets/nextjs.png" style = "width: 8%;" alt=""> '
+      }
+      if (typescript.checked == true){
+        let pl4 = document.getElementById("tech4").value;
+        res += '<img value="typescript" id="tech4" src="assets/typescript.png" style = "width: 8%;" alt="">'
+      } 
+    //   else {
+    //     res = "You have not selected anything";}
+      
+
     image = URL.createObjectURL(image)
 
     let blog = {
@@ -16,6 +49,7 @@ function addBlog() {
         image: image,
         start: start,
         end: end,
+        tech: res,
     }
 
     blogs.push(blog)
@@ -27,6 +61,9 @@ function renderBlog() {
 
     let blogContainer = document.getElementById('contents')
 
+    blogContainer.innerHTML = ""
+    
+
     for(let i = 0; i < blogs.length; i++){
         blogContainer.innerHTML += `<div class="blog-list-item">
         <div class="blog-image">
@@ -34,13 +71,14 @@ function renderBlog() {
         </div>
         <div class="blog-content">
           
-          <h1>
+          <h2>
             <a href="blog-detail.html" target="_blank">${blogs[i].title}</a>
-          </h1>
+          </h2>
           <div class="detail-blog-content">
-            <p>duration</p> ${blogs[i].start} | ${blogs[i].end} 
+            <p>duration : ${getDistanceTime(blogs[i].start,blogs[i].end)} </p> 
           </div>
           <p>${blogs[i].content} </p>
+          <p>${blogs[i].tech} </p>
           <div class="btn-group">
             <a href="#" class="btn-edit">Edit</a>
             <a href="#" class="btn-delete">Delete</a>
@@ -50,3 +88,53 @@ function renderBlog() {
     }
 }
 
+function getDistanceTime(timeStart, timeEnd) {
+  let timeOne =  new Date(timeStart)
+  let timeTwo = new Date(timeEnd)
+
+//   console.log(timeOne)
+
+  // console.log('Now: ', timeNow)
+  // console.log('Blog: ', timeBlog)
+  
+  let distance = timeTwo - timeOne // miliseconds
+  
+  console.log('distance: ', distance)
+
+    let months;
+    months = (timeTwo.getFullYear() - timeOne.getFullYear()) * 12;
+    console.log('selisih bulan', months)
+
+    months -= timeOne.getMonth();
+    console.log(timeOne.getMonth())
+    console.log(months)
+
+    months += timeTwo.getMonth();
+    console.log(timeTwo.getMonth())
+    console.log(months)
+    return months <= 0 ? 0 : months;
+
+
+  let dayDistance = Math.floor(distance / (24 * 60 * 60 * 1000 )) // convert to day
+
+  console.log('dayDistance: ', dayDistance)
+  
+//   return dayDistance
+
+//   if(dayDistance != 0) {
+//     return dayDistance + ' day ago'
+//   }else {
+//     let hourDistance = Math.floor(distance / ( 60 * 60 * 1000 ))
+//     if(hourDistance != 0) {
+//       return hourDistance + ' hours ago'
+//     } else {
+//       let minuteDistance = Math.floor(distance / ( 60 * 1000 ))
+//       if(minuteDistance != 0) {
+//         return minuteDistance + ' minute ago'
+//       } else {
+//         let secondDistance = Math.floor(distance /  1000 )
+//         return secondDistance + ' seconds ago'
+//       }
+//     }
+//   }
+}
